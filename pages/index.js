ea9484d4e1b798abb3274271fabe0f298b4ed7a9
stefanscript <sha32768@gmail.com>
2020-06-08 20:15:50 +0100

    coming soon, with logo

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,48 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
+    return (
+        <div className="container">
+            <Head>
+                <title>That Run Sauce</title>
+                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+                <link rel="apple-touch-icon" sizes="180x180"
+                      href="/apple-icon-180x180.png"/>
+                <link rel="icon" type="image/png" sizes="192x192"
+                      href="/android-icon-192x192.png"/>
+                <link rel="icon" type="image/png" sizes="32x32"
+                      href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="96x96"
+                      href="/favicon-96x96.png"/>
+                <link rel="icon" type="image/png" sizes="16x16"
+                      href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <meta name="msapplication-TileColor"
+                      content="#ffffff"/>
+                <meta name="msapplication-TileImage"
+                      content="/ms-icon-144x144.png"/>
+                <meta name="theme-color"
+                      content="#ffffff"/>
+                <meta name="description"
+                      content="Everything running, training plans for 5k, 10k, semi-marathon and marathon. "/>
+                <meta name="og:description"
+                      content="Everything running, training plans for 5k, 10k, semi-marathon and marathon."/>
+            </Head>
+            
+            <main>
+                <img src={"/GIF_5_secs_TRS.gif"} alt={"That Run Sauce"} className={"logo"}/>
+                <h3>Coming Soon</h3>
+            </main>
+            
+            <style jsx>{`
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
           justify-content: center;
@@ -70,140 +50,46 @@ export default function Home() {
         }
 
         main {
-          padding: 5rem 0;
-          flex: 1;
+           min-height: 100vh;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
+        
         .logo {
-          height: 1em;
+            max-width: 300px;
+            max-height: 60vh;
+        }
+        h3 {
+            text-transform: uppercase;
+            font-size: 1.5em;
+            color: #777;
+        }
+        
+        @media all and (min-width: 767px) {
+            .logo {
+                max-width: 400px;
+            }
         }
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
+        
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
+            
+            <style jsx global>{`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
+    
+            * {
+              box-sizing: border-box;
+            }
       `}</style>
-    </div>
-  )
+        </div>
+    )
 }
